d31b67e5e45b7abf5f81f5dcf9308d56f2cd90a1
Residentalex <a.jimenez1421@gmail.com>
2021-05-08 13:19:16 -0400

    show name chat

--- src/app/pages/activities/activities.component.ts
+++ src/app/pages/activities/activities.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { Chat } from 'src/app/models/chat';
@@ -15,19 +16,22 @@ export class ActivitiesComponent implements OnInit {
 
   uid: string = '';
 
-  chats: Observable<Chat[]>
+  Chats:Chat[] = [];
+  Persons: Person[] = [];
 
   constructor(
     private menuCtrl: MenuController,
+    private router: Router,
     private db: FirestoreService,
     private fAuth: FirebaseAuthService
   ) { }
 
   ngOnInit() {
-    this.fAuth.stateAuth().subscribe(r => {
+    this.fAuth.stateAuth().subscribe(async r => {
       this.uid = r.uid
 
-      this.getChats(r.uid);
+      this.Chats = await this.getChats(r.uid);
+      this.Persons = await this.getPersons();
     });
   }
 
@@ -35,13 +39,28 @@ export class ActivitiesComponent implements OnInit {
     this.menuCtrl.toggle();
   }
 
-  getChats(id: string) {
-    this.chats = this.db.subscribeCollectionbyParameter('ChatRooms', 'idPerson', id);
+  async getChats(id: string) {
+    const chats = await this.db.getCollectionbyParameter<Chat>('ChatRooms', 'idPerson', id);
+    return chats
   }
 
-  async getPerson(id: string) {
-    const person: Person = await this.db.getDoc('Personas', id);
-    return person
+  async getPersons(){
+    const Persons = [];
+    this.Chats.forEach( async chat => {
+      if (chat.idPerson == this.uid){
+        const dataPerson = await this.db.getDoc<Person>('Personas', chat.idTasker);
+        if (Persons.includes(dataPerson) === false) Persons.push(dataPerson);
+      }else {
+        const dataPerson = await this.db.getDoc<Person>('Personas', chat.idPerson);
+        if (Persons.includes(dataPerson) === false) Persons.push(dataPerson);
+      }
+      
+    });
+    return Persons;
+  }
+
+  openChat(id: string){
+    this.router.navigate(['/chat', id])
   }
 
 }
